0864b266798b6a2c51770569139ba0c0baefcb37
guy kaplan <guykap4@gmail.com>
2021-05-30 19:16:22 +0300

    profile starting

--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -10,7 +10,10 @@ export class Profile extends React.Component {
             <section className="profile-container">
                 <div className="profile-sidebar">
                 <div className="user-img"></div>
-                <div className="statistics"></div>
+                <div className="statistics">
+                    <div><span>stat1:</span><span>amount1</span></div>
+                    <div><span>stat2:</span><span>amount2</span></div>
+                </div>
                 <div className="side-nav"></div>
                 </div>
                 <div className="profile-main">
