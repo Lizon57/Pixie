8b03e1325ddd8a63bca1ca3933bf11ada72b8c28
O Y <orenyaniv90@gmail.com>
2021-06-02 11:24:18 +0300

    homepage guide

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -53,7 +53,7 @@ export class About extends React.Component {
                 <div className="about-page-background">
                     <section className="container">
                         <section className="about-section">
-                            <h1 className="title">About Pixie</h1>
+                            <h1 className="title first">About Pixie</h1>
                             <section className="flex wrap space-between about-pixie-container">
                                 <div className="flex column text">
                                     <h2>We are Pixie!</h2>
