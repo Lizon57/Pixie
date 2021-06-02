d73d22821d398a4b1e3d1e9cd90dd4f7ccc61e6f
AlexBil <alexbil207@gmail.com>
2021-06-02 16:06:58 +0300

    style

--- frontend/src/cmps/editor/editor-elements/elements/Text.jsx
+++ frontend/src/cmps/editor/editor-elements/elements/Text.jsx
@@ -3,24 +3,14 @@ import { utilService } from '../../../../service/util-service.js'
 export function getText() {
     return ({
         id: utilService.makeId(),
-        type: 'div',
-        role: 'text-div',
+        type: 'span',
+        role: 'childless',
         prefs: {
-            className: "text-product-container"
+            className: "text-product",
+            style: { width: '100%' }
         },
-        childs: [
-            {
-                id: utilService.makeId(),
-                type: 'span',
-                role: 'childless',
-                prefs: {
-                    className: "flex text-product",
-                    style: { width: '100%' }
-                },
-                txt: `Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eum corrupti sed pariatur dignissimos laudantium quos expedita quibusdam illo deserunt, voluptate maxime minima, magnam culpa. Perferendis nam veritatis tempora cupiditate rem!
+        txt: `Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eum corrupti sed pariatur dignissimos laudantium quos expedita quibusdam illo deserunt, voluptate maxime minima, magnam culpa. Perferendis nam veritatis tempora cupiditate rem!
                 Impedit laudantium ea recusandae excepturi sed atque culpa exercitationem fugiat nam consequuntur incidunt vitae suscipit error corrupti quam facere ratione, quos minima aliquam hic ex dolorum quia eos quae! Deleniti!
                 Atque enim quos voluptatibus debitis consequatur quo. Dolor et autem vitae itaque veritatis atque modi enim ullam, qui adipisci corrupti fugit debitis maiores exercitationem molestias impedit! Ipsa omnis assumenda accusamus?`
-            }
-        ],
     })
 }
