5f898f9bcbbd6cb7516e563a92bad1673db56a33
guy kaplan <guykap4@gmail.com>
2021-06-06 19:36:20 +0300

    template revamp

--- frontend/src/cmps/TemplatePreview.jsx
+++ frontend/src/cmps/TemplatePreview.jsx
@@ -2,17 +2,39 @@ import React from "react";
 
 export function TemplatePreview({ template, onSetData }) {
     return (
-        <div onClick={() => {
-            const web = { ...template }
-            delete web._id
-            onSetData(web)
-        }} style={{ backgroundImage: `url("${template.image}")` }} className="flex column space-between template-preview">
-            <div className="flex justify-center align-center template-name">
-                <span>{template.title}</span>
-            </div>
-            <div className="flex align-center justify-center template-hover-menu">
-                <div className="fas edit-el-icn edit-template-button"></div>
+
+        <div className="template-card-container">
+            <div onClick={() => {
+                const web = { ...template }
+                delete web._id
+                onSetData(web)
+            }}
+                className="template-card">
+                <div className="template-bar">
+                    <div className="circle"></div>
+                    <div className="circle"></div>
+                    <div className="circle"></div>
+                </div>
+                <div style={{ backgroundImage: `url("${template.image}")` }} className="template-img">
+                    <div className="template-img-hover">
+                        <button>Edit</button>
+                    </div>
+                </div>
+                <div className="template-title">{template.title}</div>
             </div>
         </div>
+
+        // <div onClick={() => {
+        //     const web = { ...template }
+        //     delete web._id
+        //     onSetData(web)
+        // }} style={{ backgroundImage: `url("${template.image}")` }} className="flex column space-between template-preview">
+        //     <div className="flex justify-center align-center template-name">
+        //         <span>{template.title}</span>
+        //     </div>
+        //     <div className="flex align-center justify-center template-hover-menu">
+        //         <div className="fas edit-el-icn edit-template-button"></div>
+        //     </div>
+        // </div>
     )
 }
